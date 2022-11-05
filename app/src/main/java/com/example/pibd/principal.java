7a54e1e344ee88b0463fc37dcbc2812130820dc1
Diana Hernandez <dhernandez15@ucol.mx>
2022-11-05 17:48:11 -0600

    Agregue el menu inferior

--- app/src/main/java/com/example/pibd/principal.java
+++ app/src/main/java/com/example/pibd/principal.java
@@ -1,30 +1,52 @@
 package com.example.pibd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class principal extends AppCompatActivity {
 
-    ImageView img;
-    Intent select;
+    //CONFIGURACION DEL MENU
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    FavoriteFragment favoriteFragment = new FavoriteFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        img = (ImageView) findViewById(R.id.xmlimg);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
 
-        img.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                select = new Intent(principal.this, post.class);
-                startActivity(select);
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.favorite:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,favoriteFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                        return true;
+                }
+
+                return false;
             }
         });
+
     }
 }
