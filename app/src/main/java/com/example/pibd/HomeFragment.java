bdfdc2ece5d96bfeffa41cdbfec6fedca60b26fd
Diana Hernandez <dhernandez15@ucol.mx>
2022-11-08 12:13:02 -0600

    Se agregó home

--- app/src/main/java/com/example/pibd/HomeFragment.java
+++ app/src/main/java/com/example/pibd/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.pibd;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class HomeFragment extends Fragment {
 
+    ImageView img;
+    Intent select;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        img=(ImageView) root.findViewById(R.id.xmlimg);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                select = new Intent(getActivity(), post.class);
+                startActivity(select);
+            }
+        });
+        return root;
     }
 }
